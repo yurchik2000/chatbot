23322bcc4a4aa905fcf351b7d33e001b937a1d02
unknown <yurchik2000@gmail.com>
2022-08-15 22:30:06 +0300

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,7 +40,10 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+.hello{
+  color: $primary-color;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -53,6 +56,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #42b983;  
 }
 </style>
